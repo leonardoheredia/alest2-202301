e04cc5e6a4853e2ce76559a24b4a41970ef65c9f
leonardoheredia <leonardo@heredia.com.br>
2023-03-06 10:53:23 -0300

    Primeiro commit. ArrayUtils, BubbleSort, InsertionSort e QuickSort.

---
+++ arrays/InsertionSort.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        for (int n = 100; n <5000 ; n+=50) {
+            int[] array1 = new int[n];
+            //ArrayUtils.preencherArrayComValoresInteirosAleatorios(array1, 1000, true);
+            //int operacoes = ordenarComInsertionSort(array1);
+            //System.out.println(n+";"+operacoes);
+        }
+    }
+    public static int ordenarComInsertionSort(int[] arrayParaOrdenar) {
+        int tamanho = arrayParaOrdenar.length;
+        int chave;
+        int operacoes = 0;
+        //IMPLEMENTAR O RESTANTE
+        return operacoes;
+    }
+}
