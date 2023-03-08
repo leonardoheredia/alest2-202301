0efbd1b0e0d778dcdc508abce6deef2db4b95b6d
Thales Veiga <thales.cveiga@gmail.com>
2023-03-08 20:33:21 -0300

    New QuickSort code

--- aula03_analise_algortimos_ordenacao/AnaliseOrdenacao.java
+++ aula03_analise_algortimos_ordenacao/AnaliseOrdenacao.java
@@ -5,27 +5,30 @@ import arrays.ArrayUtils;
 public class AnaliseOrdenacao {
 
     public static void main(String[] args) {
-        for (int n = 5; n <= 100; n++) {
+        for (int n = 5; n <= 100000; n+=50) {
             int[] meuArrayOriginal = new int[n];
             int[] meuArrayBubble = new int[n];
             int[] meuArrayInsertion = new int[n];
             int[] meuArrayMerge = new int[n];
+            int[] meuArrayQuickSort = new int[n];
 
             ArrayUtils.preencherArrayComValoresInteirosAleatorios(meuArrayOriginal, 100, true);
             ArrayUtils.clonarArray(meuArrayOriginal, meuArrayBubble);
             ArrayUtils.clonarArray(meuArrayOriginal, meuArrayInsertion);
             ArrayUtils.clonarArray(meuArrayOriginal, meuArrayMerge);
+            ArrayUtils.clonarArray(meuArrayOriginal, meuArrayQuickSort);
 
             BubbleSort bubble = new BubbleSort();
             InsertionSort insertion = new InsertionSort();
             MergeSort merge = new MergeSort();
+            QuickSort quick = new QuickSort();
 
             bubble.ordenar(meuArrayBubble);
             insertion.ordenar(meuArrayInsertion);
             merge.ordenar(meuArrayMerge);
+            quick.ordenar(meuArrayQuickSort);
 
-
-            System.out.println(n + ";" + bubble.getOperacoes() + ";" + insertion.getOperacoes() + ";" + merge.getOperacoes());
+            System.out.println(n + ";" + bubble.getOperacoes() + ";" + insertion.getOperacoes() + ";" + merge.getOperacoes() + ";" + quick.getOperacoes());
 
         }
 
